5df32b8cb67d300b12e4a70e0bb8edf5f516972b
Rvaindra7330 <haravinadona.ravindra@gmail.com>
2024-02-19 18:41:40 +0530

    added create and info components

--- src/main/java/com/example/backend/ProductController.java
+++ src/main/java/com/example/backend/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/api/products")
 public class ProductController {
 
